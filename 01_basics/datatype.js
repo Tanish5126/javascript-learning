c3a64e62b0577c96b2a2871aaf8c688f2ecaef78
Tanish Gawade <2025.tanishg@isu.ac.in>
2026-01-06 03:04:41 +0000

    Bbasics of datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treats all code as newer version
+
+// alert ( 3 + 3) // we are using nodejs, not browser
+
+
+// Types of datatypes
+
+let name = "Tanish";
+let age = 19
+let isloggedin = true
+let userstate = ""
+let city = null
+
+// Primitive Data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol uniques
+
+console.log(typeof "Tanish")
+console.log(typeof null)
