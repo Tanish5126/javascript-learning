c1aaeff042d55f81f250de357c6b3aa999772c50
Tanish Gawade <2025.tanishg@isu.ac.in>
2026-01-10 07:48:56 +0000

    Learnt about operations and conversions

---
+++ 01_basics/conversions.js
@@ -0,0 +1,38 @@
+// let score = "33";
+// console.log(typeof score);          // Prints that 'score' is a string.
+
+// let valueInNumber = Number(score); 
+// console.log(typeof valueInNumber);    // Prints that 'score' is a number.
+
+
+
+// let score1 = "33abc";
+// let valInNumber = Number(score1)     // Prints that 'score1' is a string.
+// console.log(score1);
+// console.log(valInNumber);            //Prints that 'score1' is NaN.
+
+
+
+
+// let isLoggedIn = -5;
+// let boolisLoggedIn = Boolean(isLoggedIn)
+// console.log(boolisLoggedIn);
+
+
+// // Non-zero integer -> true
+// // 0 -> false
+// // Non-empty string -> true
+// // empty string -> false
+
+
+
+
+
+// let someNumber = 33
+// let stringNumber = String ( someNumber)
+// console. log(stringNumber);
+// console. log (typeof stringNumber);
+
+
+
+
---
+++ 01_basics/operations.js
@@ -0,0 +1,40 @@
+// console.log("4"+2)
+// console.log(4+"2")
+
+// console.log("4" + (2 + 2));   //prints 44
+
+// console.log("4" + 2 + 2);     //prints 422
+// console.log(4 + 2 + "2");    // prints 62
+// console.log((4+3)*5 % 3);   //Use BODMAS rule
+
+
+// console.log(true);   //prints true
+// console.log(+true);  //prints 1
+// console.log(+"");    //prints 0
+// console.log(+false)  //prints 0
+
+
+/* Prefix and Postfix increment operator*/
+// let gameCounter = 100;
+// gameCounter++                // postfix
+// console.log(gameCounter);
+
+
+// let gameCounter1 = 100;
+// ++gameCounter1;             //prefix
+// console.log(gameCounter1);
+
+
+
+//Postfix
+// let x = 5;
+// let y = x--;  // y gets 5 (the old value), then x becomes 4
+// // Result: x = 4, y = 5
+// console.log(`x:${x}, y:${y}`);
+
+
+// //Prefix
+// let a = 3;
+// const b = ++a;
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
