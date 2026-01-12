eee775db70a90d560039c96bd7ab1b011b97aea9
Tanish Gawade <2025.tanishg@isu.ac.in>
2026-01-12 03:19:38 +0000

    started with comparsions

---
+++ 01_basics/comparsion.js
@@ -0,0 +1,6 @@
+// relational operators (<,>,<=,>=) convert 'null' to a number ie 0. '==' doesnt do that
+console.log(null > 0);    //false
+console.log(null == 0);   // false
+console.log(null >=0);    //true
+
+
