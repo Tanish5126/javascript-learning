30f2986f559772b77ff7947c331c54b49e58b4d0
Tanish Gawade <2025.tanishg@isu.ac.in>
2026-01-14 07:54:22 +0000

    Learned more about datatypes and  methods of string

---
+++ 01_basics/datatypes2.js
@@ -0,0 +1,30 @@
+//Stack (LIFO)
+// Works for primitive datatypes
+
+// let name1 = "TanishGawade";
+// let anothername = name1;
+// anothername = "TejasChavan"
+
+// console.log(name1);            // Tanish Gawade
+// console.log(anothername);      // Tejas Chavan
+
+
+
+
+
+
+// Heap 
+// Works for non-primitve datatypes
+
+// let obj1 = {
+//     name : "Tanish", 
+//     age : 19 ,
+//     weight : "60kg",
+// }
+
+// let obj2 = obj1;
+
+// obj2.name = "Tejas Chavan"
+
+// console.log(obj1);        // Tejas Chavan
+// console.log(obj2);        // Tejas Chavan
---
+++ 01_basics/strings.js
@@ -0,0 +1,21 @@
+let name1 = "Tanish Gawade";
+
+// let name2 = new String("Tanish")
+
+// console.log(typeof name1);   //string
+// console.log(typeof name2);   // object
+
+
+
+// console.log(name1.toUpperCase);       //referring to the function 
+// console.log(name1.toUpperCase());     //actually calling the function
+
+
+
+
+// const newStringOne = " Hitesh "
+// console. log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('sundar'))
