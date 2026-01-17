4dde022f16e6d619b3b12410771162d5fc0931a1
Tanish Gawade <2025.tanishg@isu.ac.in>
2026-01-17 06:46:21 +0000

    Learned basics of Date and Time in Javascript

---
+++ 01_basics/datetime.js
@@ -0,0 +1,25 @@
+// let myDate  = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());      
+
+
+
+// For customizing the date
+// let newMyDate = new Date (2026 , 0 , 6);   
+// console.log(newMyDate.toDateString()); 
+// console.log(newMyDate.toLocaleDateString()); 
+
+
+
+
+// let timeNow = new Date()
+// console.log(timeNow.getSeconds())
+// console.log(timeNow.getMinutes())
+
+
+// // Calculates milliseconds & seconds from 1st Jan 1970
+// console.log(timeNow.getTime());    
+// console.log(Math.floor(timeNow.getTime()/1000));    
