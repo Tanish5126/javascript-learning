f2edb7f90a9cf554065372e97bb541f161a5db8f
Tanish Gawade <2025.tanishg@isu.ac.in>
2026-01-19 14:53:10 +0000

    Started with loops in javascript

---
+++ 05_loops/forloop.js
@@ -0,0 +1,65 @@
+// for (var index= 0; index <=10 ; index++){
+//     console.log(index);
+    
+// }
+// console.log(index);      // Local variable cant be accessd outside the loop using let , const
+
+
+
+
+
+// for(i=0; i<=10 ; i++){
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 is best number");
+        
+//     }
+//     console.log(element)
+
+// }
+
+
+
+// for(i=0; i<=10;i++ ){
+//     console.log(`Table of : ${i}`);
+//     for(j=0; j<=10;j++ ){
+//         // console.log(`Inner loop : ${j}`);
+//         console.log(i+'*' + j+ '='+i*j);
+
+//     }
+// }
+
+
+
+// let myArray = ['apple' , 'orange' , 'grapes' , 'watermelon' , 'banana'];
+// for(let i = 0; i<=myArray.length ; i++){
+//     console.log(myArray[i]);
+    
+// }
+
+
+
+//----------------BREAK and CONTINUE------------------//
+
+// for(let i =0;i<=100;i++){
+//     if(i == 45){
+//         console.log(`${i} number detected`);
+//         break
+        
+//     }
+//     console.log(`The number is ${i}`);
+    
+// }
+
+
+
+// for(let i =0;i<=100;i++){
+//     if(i == 45){
+//         console.log(`${i} number detected`);
+//         continue
+        
+//     }
+//     console.log(`The number is ${i}`);
+    
+// }
+
