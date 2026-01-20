f16d438185e495714aa2fe56b0cb71192570f1eb
Tanish Gawade <2025.tanishg@isu.ac.in>
2026-01-20 02:07:23 +0000

    learned about while and do while loops

---
+++ 05_loops/whiledowhile.js
@@ -0,0 +1,31 @@
+// let index = 1                                     // Does not work for const
+// while(index <=10){
+//     console.log(`Value of index : ${index}`);
+//     index += 3
+    
+// }
+
+
+
+
+
+// let myArray = ['Cricket' , 'Football' , 'Volleyball' , 'Tennis']
+// let i = 0;
+
+// while(i < myArray.length){
+//     console.log(`Value is ${myArray[i]}`);
+//     i++
+    
+// }
+
+
+
+
+
+// let n = 10;
+// do {
+//     console.log(n);
+//     n++
+    
+    
+// } while (n <=100);
